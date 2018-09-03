ba6523bc3dfefdd01853783f60f6d625268223c0
John Mettraux <jmettraux@gmail.com>
2018-09-03 14:08:43 +0900

    Implement first version of Cevennes.diff

--- lib/cevennes.rb
+++ lib/cevennes.rb
@@ -12,29 +12,56 @@ module Cevennes
 
       h0 = hash(id, csv0)
       h1 = hash(id, csv1)
+
+      ks0 = h0.delete(:keys)
+      ks1 = h1.delete(:keys)
+
+      d =
+        h0
+          .collect { |k, v|
+            v1 = h1[k]
+            if v1 == v
+              [ '=', v, nil ]
+            else
+              [ v1 == nil ? '-' : '!', v, v1 ]
+            end }
+          .compact +
+        (h1.keys - h0.keys)
+          .collect { |k|
+            v = h1[k]
+            [ '+', nil, h1[k] ] }
+
+      s = d.inject({}) { |h, (a, _, _)| h[a] = (h[a] || 0) + 1; h }
+      s['l0'] = h0.length - 1
+      s['l1'] = h1.length - 1
+
+      [ [ 'keys', ks0, ks1 ], [ 'stats', s ] ] + d
     end
 
     protected
 
     def hash(id, csv)
 
-      csva = ::CSV.parse(straighten(csv))
+      csva = ::CSV.parse(reencode(csv))
         .reject { |row| row.compact.empty? }
         .drop_while { |row| ! row.include?(id) }
 
-      idi = csva[0].index(id)
+      i = csva[0].index(id)
 
-      csva.inject({}) { |h, row|
-        h[row[idi]] = deflate(row)
-        h }
+      csva[1..-1]
+        .inject({ keys: csva[0] }) { |h, row|
+          if row.compact.length > 1
+            k = row[i]
+            h[k] = row if k
+          end
+          h }
     end
 
-    def deflate(row)
+    #def deflate(row)
+    #  ::CSV.generate(encoding: 'UTF-8') { |csv| csv << row }.strip
+    #end
 
-      ::CSV.generate(encoding: 'UTF-8') { |csv| csv << row }.strip
-    end
-
-    def straighten(s)
+    def reencode(s)
 
       #s = unzip(s) if s[0, 2] == 'PK'
         # no dependency on rubyzip
